fe977285af7bd885782dcf2118fd735684f9e3d1
Rachael Hartman <rthartma@oakland.edu>
2017-01-30 13:35:27 -0500

    Ninety_Nine_Bottles_of_Beer() prints out 99 verses of song using int numBottles and for method

--- BeerSong.java
+++ BeerSong.java
@@ -7,6 +7,12 @@ public class BeerSong {
     }
     
     public static void Ninety_Nine_Bottles_of_Beer (){
+        int numBottles = 99;
+        for (int i = 99; i > 0; i--) {
+		System.out.println(numBottles + " bottles of beer on the wall, " + numBottles + " bottles of beer,");
+                numBottles = numBottles - 1;
+                System.out.println("Take one down, pass it around, " + numBottles + " bottles of beer on the wall.");
+	}
 
     }
     
