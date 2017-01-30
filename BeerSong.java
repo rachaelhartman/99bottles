82f5b1f07d3d7e096662545a4fea5a6f26ac9307
Rachael Hartman <rthartma@oakland.edu>
2017-01-30 13:39:37 -0500

    In BeerSong.java, main method now calls Ninety_Nine_Bottles_of_Beer() function

--- BeerSong.java
+++ BeerSong.java
@@ -3,7 +3,7 @@ package ninetyninebottles;
 public class BeerSong {
 
     public static void main(String[] args) {
-
+        Ninety_Nine_Bottles_of_Beer();
     }
     
     public static void Ninety_Nine_Bottles_of_Beer (){
